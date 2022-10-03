726b082e65c8c44a49cad24f300c4b894b7c31bb
sagarchandresha <sagar.bsquaresoft@gmail.com>
2022-10-03 16:42:58 +0530

    mongodb connection

--- web/middleware/custom_apis.js
+++ web/middleware/custom_apis.js
@@ -1,8 +1,36 @@
+import mongoose from "mongoose";
+
 export default function scriptTagApi(app) {
+  mongoose.connect("mongodb://localhost:27017/playing-app").then(() => console.log("working")).catch((err) => console.log(err))
+  const DetailSchema = new mongoose.Schema({
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    }
+  });
+  
+  const User = mongoose.model("user", DetailSchema);
+  const createDocument = async () => {
+    try{
+      const newUser = new User({
+        id: "2",
+        name: "Sagar Chandresha"
+      })
+      const result = await newUser.save();
+      console.log(result);
+    } catch(err){
+      console.log(err);
+    }
+  }
   app.get("/api/test-api", async (req, res) => {
+    createDocument();
     res.json({
       status: "success",
-      message: "Successfully API working",
+      message: "Data inserted successfully",
     });
   });
 }
